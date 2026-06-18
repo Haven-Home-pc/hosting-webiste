14a9e74d4296e0695964db776c02e0b2b15d6823
Alex <Haven_home_pc@outlook.com>
2026-06-18 21:28:17 +0100

    re-theme

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,17 +1,13 @@
-/** @type {import("prettier").Config} */
-const config = {
-  plugins: [
-    "prettier-plugin-astro",
-    "prettier-plugin-tailwindcss", // MUST come last
-  ],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
-};
+/** @type {import('prettier').Config} */
+export default {
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
 
-export default config;
+  plugins: ['prettier-plugin-astro'],
+
+  overrides: [{ files: '*.astro', options: { parser: 'astro' } }],
+};
